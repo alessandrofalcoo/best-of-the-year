d59e8b332288f2320915fa7176604f412ae709ae
alessandrofalcoo <alefalco.1003@gmail.com>
2025-07-31 17:40:28 +0200

    Add setter methods for title and id in Movie and Song classes; create index.html template

--- src/main/java/org/lessons/java/best_of_the_year/controller/Song.java
+++ src/main/java/org/lessons/java/best_of_the_year/controller/Song.java
@@ -16,4 +16,12 @@ public class Song {
     public int getId() {
         return id;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
